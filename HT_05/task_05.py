5dee84b20e587c6128ddd78771906eca8dd8ac18
@V-lad-K <krugonovskiy@gmail.com>
2023-10-31 10:22:04 +0200

    HT_05 attempt 2

--- HT_05/task_05.py
+++ HT_05/task_05.py
@@ -1,6 +1,7 @@
-# Ну і традиційно - калькулятор :slightly_smiling_face: Повинна бути 1 ф-цiя, яка б приймала 3 аргументи - один з яких
-# операцiя, яку зробити! Аргументи брати від юзера (можна по одному - 2, окремо +, окремо 2; можна всі разом - типу 1 + 2).
-# Операції що мають бути присутні: +, -, *, /, %, //, **. Не забудьте протестувати з різними значеннями на предмет помилок!
+# Ну і традиційно - калькулятор :slightly_smiling_face: Повинна бути 1 ф-цiя, яка б приймала 3 аргументи - один з
+# яких операцiя, яку зробити! Аргументи брати від юзера (можна по одному - 2, окремо +, окремо 2; можна всі разом -
+# типу 1 + 2). Операції що мають бути присутні: +, -, *, /, %, //, **. Не забудьте протестувати з різними значеннями
+# на предмет помилок!
 
 def calculator(number1, operation, number2):
     try:
@@ -27,8 +28,8 @@ def calculator(number1, operation, number2):
         return "division on 0 "
 
 
-operand1 = float(input("input operand1 "))
-operand2 = float(input("input operand2 "))
+operand_1 = float(input("input operand1 "))
+operand_2 = float(input("input operand2 "))
 operator = input("input operator: +, -, *, /, %, //, ** ")
 
-print(calculator(operand1, operator, operand2))
+print(calculator(operand_1, operator, operand_2))
