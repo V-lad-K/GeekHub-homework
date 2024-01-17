7c3920c436c6d12e050464b6c52a7354d6103c5f
@V-lad-K <krugonovskiy@gmail.com>
2024-01-17 06:43:41 +0200

    add app checkout

--- HT_19/mysite/products/urls.py
+++ HT_19/mysite/products/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from .views import detail_product
 from .views import products
 from .views import add_product
-from .views import checkout
 
+
+app_name = 'products'
 
 urlpatterns = [
     path('products/<str:product_id>/', detail_product, name='detail_product'),
     path('add_product/', add_product, name="add_product"),
     path('products/', products, name='products'),
-    path("checkout/", checkout, name="checkout"),
 ]
--- HT_19/mysite/products/views.py
+++ HT_19/mysite/products/views.py
@@ -25,9 +25,10 @@ def products(request):
             session_product = request.session[product_id]
             order_list = request.session.get("order") or []
 
-            found_product = next((product for product in order_list
-                                  if session_product["name"] == product["name"]),
-                                 None)
+            found_product = next(
+                (product for product in order_list
+                    if session_product["name"] == product["name"]),
+                None)
 
             if found_product:
                 found_product["quantity"] += quantity
@@ -63,40 +64,3 @@ def add_product(request):
     else:
         form = AddScrapingTaskForm()
     return render(request, "products/add_product.html", {"form": form})
-
-
-def checkout(request):
-    order_list = request.session.get("order") or []
-
-    if request.method == "POST":
-        form = AddToCheckout(request.POST)
-        if form.is_valid():
-            quantity = form.cleaned_data.get("quantity")
-            product_id = form.cleaned_data.get("product_id")
-            command = form.cleaned_data.get("command")
-
-            match command:
-                case "Add":
-                    session_product = request.session[product_id]
-                    found_product = next((product for product in order_list
-                                          if session_product["name"] == product["name"]),
-                                         None)
-
-                    if found_product:
-                        found_product["quantity"] = quantity
-                    else:
-                        order_list.append({**session_product, "quantity": quantity})
-                case "DeleteProduct":
-                    order_list = [product for product in order_list
-                                  if product["product_id"] != product_id]
-                case "DeleteAll":
-                    order_list = []
-
-        request.session["order"] = order_list
-        request.session.modified = True
-
-    order = request.session.get("order") or []
-    context = {
-        "order": order
-    }
-    return render(request, "products/checkout.html", context)
