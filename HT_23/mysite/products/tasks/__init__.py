0fa4b2e3c81c7e8495b817fa60086a7d70941ed9
@V-lad-K <krugonovskiy@gmail.com>
2024-02-09 18:16:51 +0200

    HT_23

---
+++ HT_23/mysite/products/tasks/__init__.py
@@ -0,0 +1,12 @@
+from .task_scraper import get_product_data
+from .task_save_data import save_task
+from .task_save_data import save_product_and_category
+from .task_update_data import update_product
+
+
+__all__ = [
+    "get_product_data",
+    "save_task",
+    "save_product_and_category",
+    "update_product"
+]
